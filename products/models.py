2ef7303475ce06508b3ce77b422323141377b119
retarf <retarf@gmail.com>
2019-08-29 17:11:16 +0200

    Products update

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=200, default='Product')
     description = models.TextField(blank=True)
-    price = models.FloatField(default=0.0)
+    price = models.DecimalField(max_digits=6, default=0.0, decimal_places=2)
 
     def __str__(self):
         return self.name
