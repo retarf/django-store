cd9db945c1cbb68e644bb163f78ee5746dabd238
retarf <retarf@gmail.com>
2019-09-03 19:15:41 +0200

    Django cache added.

--- store/settings.py
+++ store/settings.py
@@ -124,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'cache:11211',
+    }
+}
